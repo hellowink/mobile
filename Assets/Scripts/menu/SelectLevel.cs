2bb3079b936843aca494f1052627d3d671deb001
loumtz69 <lucianno.martinez2000@gmail.com>
2025-05-19 15:44:38 -0300

    Level selector

---
+++ Assets/Scripts/menu/SelectLevel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectLevel : MonoBehaviour
+{
+    
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Portal"))
+        {
+            SceneManager.LoadScene("LevelSelection");  // Cambia de escena
+        }
+       
+        
+    }
+}
