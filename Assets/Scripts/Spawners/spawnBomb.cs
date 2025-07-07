bdbba9f9d7aee09e72b40dbc0d76ce0443c8794d
loumtz69 <lucianno.martinez2000@gmail.com>
2025-07-07 14:38:55 -0300

    enemigo

---
+++ Assets/Scripts/Spawners/spawnBomb.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnBomb : MonoBehaviour
+{
+    public GameObject bomb;
+    public float spawnInterval = 10f;
+    public float spawnY = 10f;
+
+    private float minX, maxX;
+
+    void Start()
+    {
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, 0));
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0f, 0));
+
+        minX = leftEdge.x;
+        maxX = rightEdge.x;
+
+        InvokeRepeating(nameof(SpawnBonus), 2f, spawnInterval);
+    }
+
+    void SpawnBonus()
+    {
+        float randomX = Random.Range(minX, maxX);
+        Vector2 spawnPosition = new Vector2(randomX, spawnY);
+
+        GameObject bonus = Instantiate(bomb, spawnPosition, Quaternion.identity);
+
+        Rigidbody2D rb = bonus.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.velocity = Vector2.down * Random.Range(2f, 4f);
+        }
+    }
+}
